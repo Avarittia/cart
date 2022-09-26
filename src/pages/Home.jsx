b03271fafe2f99db251e7b9f19dd28a9f0732cbe
Nilesh Shrestha <nileshshr97@gmail.com>
2022-09-26 15:38:03 +0545

    finished

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Products from '../components/Products'
+
+const Home = () => {
+  return (
+    <div>
+        <h2>Lorem, ipsum dolor.</h2>
+        <section>
+            <h3>
+                Products
+            </h3>
+            <Products/>
+        </section>
+    </div>
+  )
+}
+
+export default Home
